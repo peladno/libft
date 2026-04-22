9ee3a53f7f867f8854c47eef9d83ec2e9c5ec194
Javier Pérez <95249621+peladno@users.noreply.github.com>
2026-04-22 21:56:24 +0900

    atoi and bzero

---
+++ libft.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jperez-u <jperez-u@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/22 21:19:02 by jperez-u          #+#    #+#             */
+/*   Updated: 2026/04/22 21:27:57 by jperez-u         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBF_H
+
+# define LIBFT_H
+
+// libraries
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+// Declare functions
+int		ft_atoi(const char *str);
+void	ft_bzero(void *s, size_t n);
+
+#endif
