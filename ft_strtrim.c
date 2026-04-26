eca2c560ed528568e5fc15b16ce9fcef8afb5abc
Javier Perez Urrutia <jperez-u@c5r1s5.42tokyo.jp>
2026-04-26 15:44:01 +0900

    done

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,13 +6,34 @@
 /*   By: jperez-u <jperez-u@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/26 14:33:50 by jperez-u          #+#    #+#             */
-/*   Updated: 2026/04/26 14:35:09 by jperez-u         ###   ########.fr       */
+/*   Updated: 2026/04/26 15:41:08 by jperez-u         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // TODO
 #include "libft.h"
 
+static int	check_set(char const c, char const *str)
+{
+	while (*str)
+	{
+		if (*str == c)
+			return (1);
+		str++;
+	}
+	return (0);
+}
+
+// return the array withouth the set charactes in the s1 adelanrte y atras del array
 char	*ft_strtrim(char const *s1, char const *set)
 {
+	int		i;
+	int		j;
+	char	*cpy;
+
+	i = 0;
+	if (!s1 || !set)
+	{
+		return (NULL);
+	}
 }
